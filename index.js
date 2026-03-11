2c972b9da7da240aa79932b12c804d3a70d31441
Joaquin Reyes <joaquinreyes2320@gmail.com>
2026-03-11 17:16:36 -0300

    remove chromium path

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    executablePath: "/usr/bin/chromium",
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",

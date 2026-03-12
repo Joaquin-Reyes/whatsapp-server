f2b16d4ae08dc73af962bc81ba6b91b142a16975
Joaquin Reyes <joaquinreyes2320@gmail.com>
2026-03-12 00:40:23 -0300

    Agregar token y phone number id

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const TOKEN = "TU_TOKEN_META";
-const PHONE_NUMBER_ID = "TU_PHONE_ID";
+const TOKEN = "EAAhJTuhZBjOQBQy2Slw3ehc9iqkZCelVSwFkY6mXUWIa0VY0ZCC98UGrHKcoRwpGOuRKgJMC2mJbEFA4S8as5kQWHXBe9AGGUKWGamjZBAgK4ZBdrcYbuBBAebSFIWbmupMNXHcqgE1a1xV3P58ZCYfSgvvZAPAbTBT9f6PodeWyt4D0Wfdd8Xb3PCGRy06GZAjJAvknK7mZC6iWm2pmpDUNS8ahSZAx6P5Xpc8F5Euw5T01IkUZAkOSRupvzxa72rL4LT9r2wBbiXz8ZBZCD05TqU6aK";
+const PHONE_NUMBER_ID = "996052293598272";
 
 app.post("/enviar", async (req, res) => {
 
